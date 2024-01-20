267cc666eaf5f4add403b9a2c15e095a381a6a67
Szymon <125824420+SciMon183@users.noreply.github.com>
2024-01-20 15:36:16 +0100

    up

--- web/script.js
+++ web/script.js
@@ -1,1 +1,20 @@
-document.write("Testowy tekst z js")
+// Pobierz guzik i modal
+var btn = document.getElementById('fnsqr');
+var modal = document.getElementById('oknofnsqr');
+
+// Dodaj obsługę zdarzenia kliknięcia na guziku
+btn.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+// Funkcja do zamykania modalu
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Dodaj obsługę zamykania modalu po kliknięciu poza nim
+window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
